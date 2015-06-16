b4cda3796dd7b84feec35395d94ab0a64d2871bf
Ruslan Kornev <oganer@gmail.com>
2015-06-16 05:57:55 +0500

    Выпилил не актуальные тесты. Тесты проходят.

--- test/controllers/collections_controller_test.rb
+++ test/controllers/collections_controller_test.rb
@@ -13,80 +13,10 @@ class CollectionsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test 'Мы должны найти данные из fixtures при просмотре Личного' do
-    sign_in users(:one)
-    get :index
-    assert_match /Тестовая коллекция/, response.body
-    assert_match /http:\/\/example.com/, response.body
-    assert_match /thumb_rails.png/, response.body
-  end
-
   test 'Мы можем удалить Collection' do
     sign_in users(:one)
     assert_difference( -> {Collection.count}, -1) do
       delete :destroy, id: collections(:one).id
-    end
-  end
-
-  test 'Мы можем создать Collection с изображением и ссылкой' do
-    sign_in users(:one)
-    assert_difference(->{CollectionItem.count}, 2) do
-      assert_difference([->{Collection.count}, ->{Link.count}, ->{Photo.count}]) do
-        post :create,
-           "collection" => {
-             "title" => "Заголовок коллекции",
-             "collection_items_attributes" => {
-               "1433692320636" => {
-                 "collectionable_type" => "Link", 
-                 "collectionable_attributes" => {
-                   "url" => "Адрес ссылки"
-                 }, 
-                 "_destroy" => "false"
-               },
-               "1433692324958" => {
-                   "collectionable_type" => "Photo",
-                   "collectionable_attributes" => {
-                     "image" => fixture_file_upload('rails.png'),
-                   "image_cache" => ""
-                   },
-                 "_destroy" => "false"
-               }
-             }
-           }
-      end
-    end
-  end
-
-  test 'Мы можем удалить изображение из Collection, при этом останется ссылка' do
-    sign_in users(:one)
-    assert_difference( ->{Photo.count}, -1 ) do
-      assert_no_difference( ->{Link.count} ) do
-        patch :update,
-         "collection" => {
-          "title" => "Тестовая коллекция",
-          "collection_items_attributes" => {
-            "0" => {
-              "collectionable_type" => "Link",
-               "collectionable_attributes" => {
-                 "url" => "http://example.com",
-                 "id" => links(:one)
-               },
-               "_destroy" => "false",
-               "id" => collection_items(:two)
-            },
-            "1" => {
-              "collectionable_type" => "Photo",
-              "collectionable_attributes" => {
-                "image_cache" => "",
-                "id" => photos(:one)
-              },
-              "_destroy" => "1",
-              "id" => collection_items(:one)
-            }
-          }
-        },
-        "id" => "980190962"
-      end
     end
   end
 
--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -4,13 +4,13 @@ class WelcomeControllerTest < ActionController::TestCase
 
   test 'Не авторизованный пользователь не должен видть ссылку Личное' do
     get :index, path: ''
-    assert_select "a", {count: 0, text: 'Личные'}
+    assert_select "a", {count: 0, text: 'Мои файлы'}
   end
 
   test 'Авторизованный пользователь должен видеть ссылку Личное' do
     sign_in users(:one)
     get :index, path: ''
-    assert_select "a", {count: 1, text: 'Личные'}
+    assert_select "a", {count: 1, text: 'Мои файлы'}
   end
 
 end
